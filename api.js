2210886b6be39f24e06724b42bd805a7408b6d00
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-06-19 00:07:10 +0530

    abc

--- api.js
+++ api.js
@@ -5,15 +5,21 @@ const cors = require("cors");
 const connectionString = process.env.MONGODB_URI;
 
 const app = express();
+
+// CORS middleware to allow requests from 'https://shubham-gaikwad.vercel.app'
 app.use(cors({
     origin: 'https://shubham-gaikwad.vercel.app'
 }));
+
+// Additional CORS headers for all routes
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://shubham-gaikwad.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
+// OPTIONS request handler for preflight requests to '/clientresponse'
 app.options('/clientresponse', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://shubham-gaikwad.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'POST');
@@ -21,10 +27,10 @@ app.options('/clientresponse', (req, res) => {
     res.status(200).send();
 });
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// POST request handler for '/clientresponse'
 app.post('/clientresponse', async (req, res) => {
     const client = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
     try {
